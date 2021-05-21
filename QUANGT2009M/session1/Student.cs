770f8e2199e99a44ecbf21ea0e88bd8e08e23b9b
nguyenhongquang2002 <71063187+nguyenhongquang2002@users.noreply.github.com>
2021-05-22 06:30:02 +0700

    a

---
+++ QUANGT2009M/session1/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QUANGT2009M.session1
+{
+    public class Student
+    {
+        public string name;
+        public int age;
+
+        public Student()
+        {
+        }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Ten SV: "+name);
+            Console.WriteLine("Tuoi: "+age);
+        }
+
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello..");
+        }
+    }
+}
